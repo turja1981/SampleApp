87c4de86db6f37f07912b39363dd7329c9543b01
turja1981 <mailturja@gmail.com>
2018-07-05 16:48:37 +0530

    dd

--- src/main/java/com/tcs/SampleApp/SampleAppApplication.java
+++ src/main/java/com/tcs/SampleApp/SampleAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SampleAppApplication {
 
 	public static void main(String[] args) {
+		// Main class
 		SpringApplication.run(SampleAppApplication.class, args);
 	}
 }
