df2f5ac3e9d4fe64a20418314944d4d36680db4d
turja <turja1981@live.com>
2018-07-10 12:48:51 +0530

    ok

--- src/test/java/com/tcs/SampleApp/NewTest.java
+++ src/test/java/com/tcs/SampleApp/NewTest.java
@@ -1,15 +1,15 @@
 package com.tcs.SampleApp;
 
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
-import java.net.URL;
 
 
 public class NewTest {
@@ -18,20 +18,27 @@ public class NewTest {
     
 	@Test				
 	public void testEasy() throws Exception{
+//		System.setProperty("webdriver.chrome.driver", "/home/turja/DevOps/chromedriver");
+		ChromeOptions options = new ChromeOptions();
+//		// set some options
+		DesiredCapabilities dc = DesiredCapabilities.chrome();
+		dc.setCapability(ChromeOptions.CAPABILITY, options);
+		driver = new RemoteWebDriver(new URL("http://localhost:5556/wd/hub"), dc);
+		System.out.println("inside test");
 		
-		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), DesiredCapabilities.chrome());
-	    
-		driver.get("http://localhost:8080/");  
+//		driver = new ChromeDriver();
+		driver.get("http://127.0.01:8080/");  
 		String title = driver.getTitle();				 
 		Assert.assertTrue(title.contains("Home")); 		
 	}	
 	@BeforeTest
 	public void beforeTest() {	
 
+
 	}		
 	@AfterTest
 	public void afterTest() {
-		driver.quit();			
+		driver.quit();	
 	}		
 }	
 
