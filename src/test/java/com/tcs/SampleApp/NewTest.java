e7e9b9c672b488786e272ad1255d377ad16ae4d9
turja <turja1981@live.com>
2018-07-10 09:03:58 +0530

    ok

--- src/test/java/com/tcs/SampleApp/NewTest.java
+++ src/test/java/com/tcs/SampleApp/NewTest.java
@@ -2,24 +2,32 @@ package com.tcs.SampleApp;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.net.URL;
+
+
 public class NewTest {
 
     private WebDriver driver;		
+    
 	@Test				
-	public void testEasy() {	
+	public void testEasy() throws Exception{
+		
+		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), DesiredCapabilities.chrome());
+	    
 		driver.get("http://localhost:8080/");  
 		String title = driver.getTitle();				 
 		Assert.assertTrue(title.contains("Home")); 		
 	}	
 	@BeforeTest
 	public void beforeTest() {	
-		System.setProperty("webdriver.chrome.driver", "/home/turja/DevOps/chromedriver");		
-	    driver = new ChromeDriver();  
+
 	}		
 	@AfterTest
 	public void afterTest() {
